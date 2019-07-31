8437ed60fa3f355b39bbc22c6e786d33b485851f
Ubuntu <ubuntu@ip-172-31-9-16.eu-west-1.compute.internal>
2019-07-31 22:23:14 +0000

    First bar chart render

--- static/js/graph.js
+++ static/js/graph.js
@@ -8,9 +8,24 @@ function makeGraphs(error,salaryData) {
  
  show_gender_balance(ndx);
  
+ dc.renderAll();
+ 
 }
 
 function show_gender_balance(ndx){
-    var dim = ndx.dimensions(dc.pluck("sex"));
-    var group 
-}
+    var dim = ndx.dimensions(dc.pluck('sex'));
+    var group = dim.group();
+    
+    dc.barChart("#gender-balance")
+    -width(400)
+    -height(300)
+    -margins({top:10, right:50, bottom:30, left:50})
+    -dimension(dim)
+    -group(group)
+    -transitionDuration (500)
+    -x(d3.scale.ordinal())
+    -elastic(true)
+    -xAxisLabel("Gender")
+    -yAxis().ticks(20);
+    }
+    
